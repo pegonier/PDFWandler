b4f71f5cdce64e5d187a045efd3edc27a8b8c23d
pegonier <gaephi79@gmail.com>
2025-01-06 22:26:01 +0100

    PDFWandler 6.1.2025 zu exe

--- src/main/java/org/pegonier/pdfwandler/CheckInFolder.java
+++ src/main/java/org/pegonier/pdfwandler/CheckInFolder.java
@@ -1,24 +1,8 @@
 package org.pegonier.pdfwandler;
 import java.io.File;
-import java.util.Arrays;
 
 public class CheckInFolder {
     public String [] listDir(File dir) {
-
-        File[] files = dir.listFiles();
-        String [] fileList = dir.list();
-
-        /*if (files != null) { // Erforderliche Berechtigungen etc. sind vorhanden
-            for (File file : files) {
-                System.out.print(file.getName());
-                if (file.isDirectory()) {
-                    System.out.print(" (Ordner)\n");
-                    listDir(file);
-                } else {
-                    System.out.print(" (Datei)\n");
-                }
-            }
-        }*/
-        return fileList;
+        return dir.list();
     }
 }
